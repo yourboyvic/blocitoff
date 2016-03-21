67628b6b133bccebc6951163b56940ad74dac46b
vic kim <yourboyvic@gmail.com>
2016-03-21 16:02:58 -0700

    create todos added after devise login

---
+++ blocitoff/README.md
@@ -0,0 +1,4 @@
+== README
+
+2016 02 26
+  This is the first commit and serves as the build log for Blocitoff
---
+++ blocitoff/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :tasks
+  devise_for :users
+
+  root :to => "tasks#index"
+
+end
